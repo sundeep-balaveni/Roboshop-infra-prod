0d948de88ed10a4ca1034ced3155dbfb22f2b4bf
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-18 15:39:47 +0530

    sdsdwrqr

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 dnf install ansible -y
+
+cd /home/ec2-user
+
+git clone https://github.com/sundeep-balaveni/Ansible-role-roboshop-tf.git
+
+
+cd Ansible-roboshop-role-tf
+
+ansible-playbook -i inventory.ini mongo.yaml
+
