974e771587e6e396e46b97055de998506e4b1983
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-18 16:04:52 +0530

    affafeqreqq

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,7 +7,7 @@ cd /home/ec2-user
 git clone https://github.com/sundeep-balaveni/Ansible-role-roboshop-tf.git
 
 
-cd Ansible-roboshop-role-tf
+cd Ansible-role-roboshop-tf
 
 ansible-playbook -i inventory.ini mongo.yml
 
