f562a1263ce5ebada602fb3f1631d87a0e9ea3b9
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-18 20:06:51 +0530

    sfdafesdsxcd

--- 40-databases/mysql.tf
+++ 40-databases/mysql.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "mysql" {
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_ssm_parameter.mysql_sg_id.value]
   subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-  tags = { Name = var.instance_name }
+  tags = { Name = var.instance_name[2] }
 
 }
 
