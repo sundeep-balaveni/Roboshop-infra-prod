264ce0ffdad6619e83a7514186f8342c901e53c4
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-18 16:38:00 +0530

    asfafee

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -4,11 +4,11 @@ dnf install ansible -y
 
 cd /home/ec2-user
 
-git clone https://github.com/sundeep-balaveni/Ansible-role-roboshop-tf.git
+# Remove old clone to always get fresh code
+rm -rf Ansible-role-roboshop-tf
 
-git pull https://github.com/sundeep-balaveni/Ansible-role-roboshop-tf.git
+git clone https://github.com/sundeep-balaveni/Ansible-role-roboshop-tf.git
 
 cd Ansible-role-roboshop-tf
 
 ansible-playbook -i inventory.ini mongo.yml
-
