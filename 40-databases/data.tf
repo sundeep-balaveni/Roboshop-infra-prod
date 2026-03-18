8ef66282f4715dc0e4586bc9f5430016ad66b29a
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-18 12:58:43 +0530

    dsjd

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -1,3 +1,7 @@
 data "aws_ssm_parameter" "database_sg_id" {
      name  = "${var.project}-${var.env}-database"
 }
+
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "${var.project}-${var.env}-database-subnets"
+}
