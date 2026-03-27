d684c3fe7811735a402d9ad3ec596bd8dd9b6973
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-03-27 15:30:11 +0530

    fdfiujirmmmnjjjjjnllkkklll

--- 50-Backend/catalogue.sh
+++ 50-Backend/catalogue.sh
@@ -1,20 +1,39 @@
+# #!/bin/bash
+
+# dnf install ansible -y
+
+# cd /home/ec2-user
+
+# # Remove old clone to always get fresh code
+# sudo rm -rf Ansible-role-roboshop-tf
+
+# git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
+
+# git pull 
+
+# cd ansible-roboshop-roles-tf
+
+
+# #ds
+
+
+
+
+# ansible-playbook -i inventory.ini catalogue.yml  -vvv  
+
+
+
+
 #!/bin/bash
 
+component=$1
+environment=$2
+app_version=$3
 dnf install ansible -y
 
 cd /home/ec2-user
-
-# Remove old clone to always get fresh code
-sudo rm -rf Ansible-role-roboshop-tf
-
 git clone https://github.com/daws-88s/ansible-roboshop-roles-tf.git
 
-git pull 
-
 cd ansible-roboshop-roles-tf
-
-
-#ds
-
-
-ansible-playbook -i inventory.ini catalogue.yml  -vvv  
+git pull
+ansible-playbook -e component=$component -e env=$environment -e app_version=$app_version roboshop.yaml
