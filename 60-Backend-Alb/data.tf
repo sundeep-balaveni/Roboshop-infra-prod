17ffc596cbb86d40d8c11d427151a570375612be
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-04-10 17:32:28 +0530

    added s3 d

--- 60-Backend-Alb/data.tf
+++ 60-Backend-Alb/data.tf
@@ -7,9 +7,7 @@ data "aws_ssm_parameter" "backend_alb_sg_id" {
      name  = "${var.project}-${var.env}-Backend_alb"
 }
 
-data "aws_ssm_parameter" "vpc_id" {
-     name  = "${var.project}-${var.env}"
-}
+
 
 
 data "aws_ssm_parameter" "vpc_id" {
