4873eb8344f4668012b79a1ff4f35484d0812051
1sandeep420 <SANDEEPBALAVENI2000@GMAIL.COM>
2026-04-10 17:57:23 +0530

    sdhdk0dddd

--- 50-Backend/parameter.tf
+++ 50-Backend/parameter.tf
@@ -3,3 +3,10 @@ resource "aws_ssm_parameter" "catalogue_id" {
   type  = "String"
   value = aws_instance.catalogue.id
 }
+
+
+resource "aws_ssm_parameter" "Catalouge_ami_id" {
+  name  = "${var.project}-${var.env}-catalogue_ami_id"
+  type  = "String"
+  value = aws_ami_from_instance.Catalogue.id
+}
